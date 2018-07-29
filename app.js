67a5152bedc279abc3fe8121b9e7da7dd3cecb31
andrewjap90 <andrew.japardi@gmail.com>
2018-07-29 14:37:34 +0700

    update shopping cart

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var expressHbs = require('express-handlebars');
 var mongoose = require('mongoose');
+var session = require('express-session');
+var passport = require('passport');
+var flash = require('connect-flash');
+var validator = require('express-validator');
+var MongoStore = require('connect-mongo')(session);
 
 var indexRouter = require('./routes/index');
+var userRoutes = require('./routes/user');
 
 var app = express();
 
 mongoose.connect('mongodb://localhost:27017/shopping');
+require('./config/passport');
 
 // view engine setup
 app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
@@ -19,9 +26,25 @@ app.set('view engine', '.hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
+app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.locals.login = req.isAuthenticated();
+  next();
+});
+
+app.use(function(req, res, next) {
+  res.locals.session = req.session;
+  next();
+});
+
+app.use('/user', userRoutes);
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
 var formidable = require('formidable');
+var Cart = require('../models/cart');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,6 +14,45 @@ router.get('/', function(req, res, next) {
         }
         res.render('shop/index', { title: 'Shopping Cart', products: productChunks });
     });
+});
+
+router.get('/add-to-cart/:id', function (req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+  Product.findById(productId, function (err, product) {
+    cart.add(product, product.id);
+    req.session.cart= cart;
+    res.redirect('/');
+  });
+});
+
+router.get('/reduce/:id', function (req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+  cart.reduceByOne(productId);
+  req.session.cart = cart;
+  res.redirect('/shopping-cart');
+});
+
+router.get('/remove/:id', function (req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+  cart.removeItem(productId);
+  req.session.cart = cart;
+  res.redirect('/shopping-cart');
+});
+
+router.get('/shopping-cart', function (req, res, next) {
+  if (!req.session.cart) {
+      return res.render('shop/shopping-cart', {product: null});
+  }
+  var cart = new Cart(req.session.cart);
+  res.render('shop/shopping-cart',
+  {products: cart.generateArray(),
+    totalPrice: cart.totalPrice});
 });
 
 router.get('/addNewProduct', function(req, res) {
